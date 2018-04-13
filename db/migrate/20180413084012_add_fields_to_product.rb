74ec29fe3ff311362870d9efcbe1a9858d97b69b
Alexander Spanos <alexisspa9@gmail.com>
2018-04-13 08:40:38 +0000

    automatically backed up by learn

--- db/migrate/20180413084012_add_fields_to_product.rb
+++ db/migrate/20180413084012_add_fields_to_product.rb
@@ -1,4 +1,4 @@
-class AddFieldsToProducts < ActiveRecord::Migration
+class AddFieldsToProduct < ActiveRecord::Migration
   def change
     add_column :products, :inventory, :integer
     add_column :products, :description, :string
